d3c41af3d152450ca5dd20a04aa5ca1078fcebd5
Biboswan Roy <biboswan98@gmail.com>
2020-10-19 23:17:04 +0530

    Optimising svg and remove relative transforms

--- components/Svgs/Logo.jsx
+++ components/Svgs/Logo.jsx
@@ -1,163 +1,132 @@
 function Logo(props) {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsOsb="http://www.openswatchbook.org/uri/2009/osb"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            version="1.1"
-            viewBox="0 0 200 200"
-            {...props}
-        >
-            <defs>
-                <linearGradient id="linearGradient2218">
-                    <stop offset="0" stopColor="#ff8f44" stopOpacity="0.843"></stop>
-                    <stop offset="1" stopColor="#242cf6" stopOpacity="0.898"></stop>
-                </linearGradient>
-                <linearGradient osbPaint="solid">
-                    <stop offset="0" stopColor="#adc8e5" stopOpacity="1"></stop>
-                </linearGradient>
-                <path d="M55.94 194.28H151.946V264.584H55.94z"></path>
-                <path d="M55.94 194.28H151.946V264.584H55.94z"></path>
-                <path d="M55.94 194.28H151.946V264.584H55.94z"></path>
-                <path d="M55.94 194.28H151.946V264.584H55.94z"></path>
-                <path id="rect1455-7-4" d="M55.94 194.28H151.946V264.584H55.94z"></path>
-                <path d="M55.94 194.28H151.946V264.584H55.94z"></path>
-                <linearGradient
-                    id="linearGradient2212"
-                    x1="56.924"
-                    x2="81.626"
-                    y1="208.014"
-                    y2="209.309"
-                    gradientUnits="userSpaceOnUse"
-                    xlinkHref="#linearGradient2218"
-                ></linearGradient>
-                <linearGradient
-                    id="linearGradient2226"
-                    x1="-2.215"
-                    x2="9.506"
-                    y1="89.87"
-                    y2="90.485"
-                    gradientTransform="matrix(2.41413 0 0 3.04012 -330.162 -235.874)"
-                    gradientUnits="userSpaceOnUse"
-                    xlinkHref="#linearGradient2218"
-                ></linearGradient>
-                <linearGradient
-                    id="linearGradient2234"
-                    x1="15.875"
-                    x2="18.521"
-                    y1="87.146"
-                    y2="87.285"
-                    gradientTransform="matrix(2.41413 0 0 3.04012 -330.162 -235.874)"
-                    gradientUnits="userSpaceOnUse"
-                    xlinkHref="#linearGradient2218"
-                ></linearGradient>
-                <linearGradient
-                    id="linearGradient2242"
-                    x1="24.927"
-                    x2="36.638"
-                    y1="89.849"
-                    y2="90.463"
-                    gradientTransform="matrix(2.41413 0 0 3.04012 -330.162 -235.874)"
-                    gradientUnits="userSpaceOnUse"
-                    xlinkHref="#linearGradient2218"
-                ></linearGradient>
-                <linearGradient
-                    id="linearGradient2250"
-                    x1="50.696"
-                    x2="65.614"
-                    y1="13.61"
-                    y2="14.391"
-                    gradientTransform="matrix(2.41413 0 0 3.04012 -389.921 14.64)"
-                    gradientUnits="userSpaceOnUse"
-                    xlinkHref="#linearGradient2218"
-                ></linearGradient>
-                <linearGradient
-                    id="linearGradient2258"
-                    x1="-6.461"
-                    x2="8.457"
-                    y1="73.367"
-                    y2="74.148"
-                    gradientTransform="matrix(2.41413 0 0 3.04012 -329.613 -166.512)"
-                    gradientUnits="userSpaceOnUse"
-                    xlinkHref="#linearGradient2218"
-                ></linearGradient>
-            </defs>
-            <g fillOpacity="1" transform="translate(345.21 -6.596)">
-                <path
-                    d="M-310.213 61.05l-1.017.691h-31.707l-1.136-.69-1.137-.69V55.078l1.137-.69 1.136-.69h15.496c16.534 0 16.99.03 17.887 1.17.197.25.358 1.588.358 2.973 0 2.467-.02 2.531-1.017 3.209z"
-                    style={{ mixBlendMode: 'normal' }}
-                    fill="url(#linearGradient2258)"
-                    stroke="none"
-                    strokeWidth="0.287"
-                ></path>
-                <text
-                    fill="url(#linearGradient2212)"
-                    fontFamily="Chalkboard"
-                    fontSize="25.4"
-                    fontStretch="normal"
-                    fontStyle="normal"
-                    fontVariant="normal"
-                    fontWeight="normal"
-                    transform="matrix(7.49303 0 0 8.88916 -756.834 -1728.313)"
-                    xmlSpace="preserve"
-                    style={{
-                        lineHeight: '1.25',
-                        InkscapeFontSpecification: "'Chalkboard, Normal'",
-                        fontVariantLigatures: 'normal',
-                        fontVariantCaps: 'normal',
-                        fontVariantNumeric: 'normal',
-                        fontVariantEastAsian: 'normal',
-                        whiteSpace: 'pre',
-                        shapeInside: 'url(#rect1455-7-4)',
-                        mixBlendMode: 'normal',
-                    }}
-                >
-                    <tspan x="55.941" y="217.165">
-                        <tspan
-                            style={{
-                                InkscapeFontSpecification: "'Chalkboard, Normal'",
-                                fontVariantLigatures: 'normal',
-                                fontVariantCaps: 'normal',
-                                fontVariantNumeric: 'normal',
-                                fontVariantEastAsian: 'normal',
-                            }}
-                            fontSize="25.4"
-                        >
-                            Br
-                        </tspan>
-                    </tspan>
-                </text>
-                <path
-                    style={{ mixBlendMode: 'normal' }}
-                    fill="url(#linearGradient2242)"
-                    stroke="none"
-                    strokeWidth="0.287"
-                    d="M-269.813 51.07l.331-1.394 11.21-14.117 11.21-14.117 1.191-.523 1.191-.524 1.483 1.868 1.484 1.867-.416 1.5-.416 1.5-10.958 13.799c-11.69 14.723-12.03 15.107-13.304 15.1-.28-.002-1.145-.804-1.923-1.783-1.385-1.745-1.407-1.809-1.083-3.175z"
-                ></path>
-                <path
-                    d="M-291.289 50.667l-.549-1.281V9.458l.548-1.431.549-1.431h4.194l.548 1.43.549 1.432v19.514c0 20.821-.024 21.394-.93 22.525-.198.248-1.26.45-2.36.45-1.96 0-2.01-.025-2.549-1.28z"
-                    style={{ mixBlendMode: 'normal' }}
-                    fill="url(#linearGradient2234)"
-                    stroke="none"
-                    strokeWidth="0.287"
-                ></path>
-                <path
-                    style={{ mixBlendMode: 'normal' }}
-                    fill="url(#linearGradient2226)"
-                    stroke="none"
-                    strokeWidth="0.287"
-                    d="M-311.151 55.861l-1.108-.417-11.21-14.116-11.21-14.117-.416-1.5-.415-1.5 1.483-1.868 1.483-1.867 1.19.523 1.192.524 10.957 13.799c11.692 14.723 11.997 15.15 11.991 16.754 0 .353-.638 1.442-1.416 2.422-1.385 1.744-1.436 1.771-2.521 1.363z"
-                ></path>
-                <path
-                    style={{ mixBlendMode: 'normal' }}
-                    fill="url(#linearGradient2250)"
-                    stroke="none"
-                    strokeWidth="0.287"
-                    d="M-266.517 60.533l1.017.692h31.707l1.136-.69 1.137-.691v-5.282l-1.137-.69-1.136-.691h-15.496c-16.534 0-16.99.03-17.886 1.17-.198.25-.359 1.588-.359 2.973 0 2.467.02 2.531 1.017 3.209z"
-                ></path>
-            </g>
-        </svg>
-    );
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 200 200"
+      {...props}
+    >
+      <defs>
+        <linearGradient id="linearGradient2218">
+          <stop offset="0" stopColor="#ff8f44" stopOpacity="0.843"></stop>
+          <stop offset="1" stopColor="#242cf6" stopOpacity="0.898"></stop>
+        </linearGradient>
+        <linearGradient
+          id="linearGradient2212"
+          x1="56.924"
+          x2="81.626"
+          y1="208.01"
+          y2="209.31"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient2218"
+        ></linearGradient>
+        <linearGradient
+          id="linearGradient2226"
+          x1="-2.215"
+          x2="9.506"
+          y1="89.87"
+          y2="90.485"
+          gradientTransform="matrix(2.4141 0 0 3.0401 15.047 -242.47)"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient2218"
+        ></linearGradient>
+        <linearGradient
+          id="linearGradient2234"
+          x1="15.875"
+          x2="18.521"
+          y1="87.146"
+          y2="87.285"
+          gradientTransform="matrix(2.4141 0 0 3.0401 15.047 -242.47)"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient2218"
+        ></linearGradient>
+        <linearGradient
+          id="linearGradient2242"
+          x1="24.927"
+          x2="36.638"
+          y1="89.849"
+          y2="90.463"
+          gradientTransform="matrix(2.4141 0 0 3.0401 15.047 -242.47)"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient2218"
+        ></linearGradient>
+        <linearGradient
+          id="linearGradient2250"
+          x1="50.696"
+          x2="65.614"
+          y1="13.61"
+          y2="14.391"
+          gradientTransform="matrix(2.4141 0 0 3.0401 -44.712 8.044)"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient2218"
+        ></linearGradient>
+        <linearGradient
+          id="linearGradient2258"
+          x1="-6.461"
+          x2="8.457"
+          y1="73.367"
+          y2="74.148"
+          gradientTransform="matrix(2.4141 0 0 3.0401 15.597 -173.11)"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient2218"
+        ></linearGradient>
+      </defs>
+      <path
+        fill="url(#linearGradient2258)"
+        d="M34.996 54.454l-1.017.692H2.272l-2.273-1.381v-5.282l2.273-1.381h15.496c16.534 0 16.989.03 17.887 1.17.197.25.358 1.588.358 2.973 0 2.467-.02 2.531-1.017 3.21z"
+        style={{ mixBlendMode: "normal" }}
+      ></path>
+      <text
+        fill="url(#linearGradient2212)"
+        fontFamily="Chalkboard"
+        fontSize="25.4"
+        transform="matrix(7.493 0 0 8.8892 -411.62 -1734.9)"
+        style={{
+          fontVariantCaps: "normal",
+          fontVariantEastAsian: "normal",
+          fontVariantLigatures: "normal",
+          fontVariantNumeric: "normal",
+          lineHeight: "1.25",
+          mixBlendMode: "normal",
+          shapeInside: "url(#rect1455-7-4)",
+          whiteSpace: "pre",
+        }}
+      >
+        <tspan x="55.941" y="217.165">
+          <tspan
+            style={{
+              fontVariantCaps: "normal",
+              fontVariantEastAsian: "normal",
+              fontVariantLigatures: "normal",
+              fontVariantNumeric: "normal",
+            }}
+          >
+            Br
+          </tspan>
+        </tspan>
+      </text>
+      <path
+        fill="url(#linearGradient2242)"
+        d="M75.397 44.475l.33-1.395 22.42-28.234 2.383-1.047 2.966 3.735-.832 3-10.957 13.798c-11.69 14.723-12.03 15.107-13.305 15.1-.28-.001-1.145-.804-1.922-1.783-1.386-1.745-1.407-1.809-1.083-3.175z"
+        style={{ mixBlendMode: "normal" }}
+      ></path>
+      <path
+        fill="url(#linearGradient2234)"
+        d="M53.921 44.071l-.55-1.28V2.861l.549-1.43L54.468 0h4.195l1.096 2.862v19.514c0 20.821-.023 21.395-.929 22.525-.198.248-1.26.451-2.36.451-1.96 0-2.01-.025-2.549-1.28z"
+        style={{ mixBlendMode: "normal" }}
+      ></path>
+      <path
+        fill="url(#linearGradient2226)"
+        d="M34.058 49.265l-1.107-.417-22.42-28.234-.416-1.5-.416-1.5 1.483-1.867 1.483-1.868 2.382 1.048 10.957 13.798C37.695 43.448 38.001 43.874 37.995 45.48c-.001.352-.638 1.442-1.416 2.42-1.385 1.746-1.436 1.773-2.521 1.364z"
+        style={{ mixBlendMode: "normal" }}
+      ></path>
+      <path
+        fill="url(#linearGradient2250)"
+        d="M78.693 53.937l1.017.692h31.707l2.273-1.381v-5.282l-2.273-1.381H95.921c-16.534 0-16.989.03-17.887 1.17-.197.25-.358 1.588-.358 2.973 0 2.467.02 2.531 1.017 3.21z"
+        style={{ mixBlendMode: "normal" }}
+      ></path>
+    </svg>
+  );
 }
 
-export default React.memo(Logo);
+export default Logo;
