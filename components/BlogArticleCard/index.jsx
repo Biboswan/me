0512042acb5d9d5365392cd82a27c3ad8c1938fc
Biboswan Roy <biboswan98@gmail.com>
2020-10-24 17:56:01 +0530

    Add copy url - blog article

--- components/BlogArticleCard/index.jsx
+++ components/BlogArticleCard/index.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Sub1, Body2, Small } from 'components/Font';
+import useToggle from 'custom-hooks/useToggle';
+import { useCallback } from 'react';
 
 const Container = styled.section`
     border: 1px solid #000000;
@@ -21,6 +23,13 @@ const TextInfo = styled.div`
 
     a {
         text-decoration: none;
+
+        h2 :hover {
+            background: ${props =>
+                `-webkit-linear-gradient(${props.theme.color.brand}, ${props.theme.color.secondary})`};
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
     }
 
     .intro {
@@ -31,12 +40,58 @@ const TextInfo = styled.div`
     }
 `;
 
+const ReadAndCopy = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
 const Read = styled(Small)`
     @media only screen and (min-width: ${props => props.theme.breakpoint.sm}px) {
         font-size: 1rem;
         font-weight: ${props => props.theme.font_weight.semibold};
     }
 `;
+
+const Button = styled.button`
+    background-color: ${props => props.theme.color.blue[900]};
+    color: ${props => props.theme.color.white};
+    border-radius: 8px;
+    min-width: 48px;
+    min-height: 48px;
+    box-sizing: border-box;
+    padding: ${props => props.theme.base_spacing * 2}px ${props => props.theme.base_spacing * 6}px;
+    cursor: pointer;
+    border: none;
+
+    &:hover {
+        background-color: ${props => props.theme.color.secondary};
+    }
+`;
+
+const CopyURLButton = ({ textToCopy, ...rest }) => {
+    const [isCopied, toggleCopied] = useToggle();
+
+    const handleCopy = useCallback(async () => {
+        try {
+            await navigator.clipboard.writeText(textToCopy);
+            toggleCopied();
+            setTimeout(toggleCopied, 300);
+        } catch (err) {
+            //
+        }
+    }, []);
+
+    return (
+        <Button onClick={handleCopy} {...rest}>
+            {isCopied ? 'Copied!' : 'Copy URL'}
+        </Button>
+    );
+};
+
+CopyURLButton.propTypes = {
+    textToCopy: PropTypes.string.isRequired,
+};
 
 const BlogArticleCard = ({ thumbnail, link, title, startingLines, readTime, ...rest }) => {
     return (
@@ -55,9 +110,12 @@ const BlogArticleCard = ({ thumbnail, link, title, startingLines, readTime, ...r
                 <Body2 className="intro" as="p">
                     {startingLines}
                 </Body2>
-                <Read as="h4" isItalic weight="bold">
-                    Read Time: {readTime}mins
-                </Read>
+                <ReadAndCopy>
+                    <Read as="h4" isItalic weight="bold">
+                        Read Time: {readTime}mins
+                    </Read>
+                    <CopyURLButton textToCopy={link} />
+                </ReadAndCopy>
             </TextInfo>
         </Container>
     );
